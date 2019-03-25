9091c422fe36f702b1a6ed86a8176cfd7386321c
Masha Blair <mashablair@gmail.com>
2019-03-25 16:38:38 -0700

    Implement conditional message when all movies are deleted.

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,11 +6,33 @@ class Movies extends Component {
     movies: getMovies()
   };
 
+  handleDelete = movie => {
+    console.log(movie);
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState(prevState => ({ movies }));
+  };
+
+  renderMessage(count) {
+    // const length = this.state.movies.length;
+    if (count > 0) {
+      return `Showing ${count} movies in the database.`;
+    } else {
+      return "There are no movies.";
+    }
+  }
+
   render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) {
+      return <h2 className="movies-header">There are no movies.</h2>;
+    }
     return (
       <React.Fragment>
         <h2 className="movies-header">
-          Showing {this.state.movies.length} movies in the database.
+          {/* {this.state.movies.length > 0
+            ? `Showing ${this.state.movies.length} movies in the database.`
+            : `There are no movies`} */}
+          {this.renderMessage(count)}
         </h2>
         <table className="table table-striped">
           <thead>
@@ -30,7 +52,12 @@ class Movies extends Component {
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
                 <td>
-                  <button className="btn btn-danger">Delete</button>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
